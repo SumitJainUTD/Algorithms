75f4f26b2e9bf7b7654c0198a5535cd7e4c05dc1
Sumit <sumi12june@gmail.com>
2019-01-04 22:57:38 -0600

    LRU code corrected

--- src/Sep2018/LRU/LRU.java
+++ src/Sep2018/LRU/LRU.java
@@ -6,38 +6,39 @@ import java.util.*;
  * Created by sjain on 9/9/17.
  */
 public class LRU {
+    LinkedHashSet<Integer> set;
+    Deque<Integer> cache;
     int capacity;
-    LinkedHashMap<Integer,Integer> cache;
-    Queue<Integer> queue;
 
     public LRU(int capacity) {
         this.capacity = capacity;
-        cache = new LinkedHashMap<Integer,Integer>(capacity);
-        queue = new LinkedList<Integer>();
+        set = new LinkedHashSet<>(capacity);
+        cache = new LinkedList<>();
     }
 
-    public int get(int key) {
-        if(cache.containsKey(key)){
-            queue.remove(key);
-            queue.add(key);
-            return cache.get(key);
-        }
-        return -1;
-    }
-
-    public void put(int key, int value) {
-        if(cache.size()==capacity){
-            int oldestKey = queue.poll();
-            cache.remove(oldestKey);
+    public void look(int key) {
+        //check if it is already present in cache (deque)
+        if(set.contains(key)){
+            //remove it from cache so that it can be added at front of the cache
+            cache.remove(key);
         }else{
-            queue.add(key);
+            //not already in cache, adding it
+            if(cache.size()==capacity){
+                //remove the last one from cache
+                cache.removeLast();
+                set.remove(key);
+            }
         }
-        cache.put(key,value);
+        //add it to cache
+        cache.push(key);
+        set.add(key);
+        System.out.println("Looking for key: " + key);
+        printCache();
     }
 
-    public void print(){
-        Set set = cache.keySet();
-        Iterator<Integer> iterator = set.iterator();
+    public void printCache(){
+        System.out.print("Cache: ");
+        Iterator<Integer> iterator = cache.iterator();
         while(iterator.hasNext()){
             System.out.print(iterator.next() + " ");
         }
@@ -45,17 +46,16 @@ public class LRU {
     }
 
     public static void main(String[] args) {
-        int capacity = 3;
+        int capacity = 4;
         LRU lru = new LRU(capacity);
-        lru.put(1,1);
-        lru.put(2,2);
-        System.out.println(lru.get(2));
-        lru.put(3,3);
-//        lru.print();
-        lru.put(4,4);
-        lru.print();
-        lru.get(3);
-        lru.put(5,5);
-        lru.print();
+        lru.look(1);
+        lru.look(2);
+        lru.look(1);
+        lru.look(3);
+        lru.look(4);
+        lru.look(3);
+        lru.look(5);
+        lru.look(4);
+        lru.look(6);
     }
 }
